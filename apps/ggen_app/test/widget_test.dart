c90894f2a40d2fe2d771a880294ff2769916670a
Sobuj <56004845+soobujmiah@users.noreply.github.com>
2026-08-19 22:14:11 +0600

    fix(test): remove duplicate immersive test block\n\nSigned-off-by: Sobuj <soobujmiah@users.noreply.github.com>

--- apps/ggen_app/test/widget_test.dart
+++ apps/ggen_app/test/widget_test.dart
@@ -44,18 +44,3 @@ void main() {
   });
 
 }
-
-
-  testWidgets('can enter and leave immersive canvas mode', (tester) async {
-    tester.view.physicalSize = const Size(471, 1020);
-    tester.view.devicePixelRatio = 1;
-    addTearDown(tester.view.reset);
-    await tester.pumpWidget(const GgenApp());
-    await tester.tap(find.byTooltip('Immersive canvas'));
-    await tester.pumpAndSettle();
-    expect(find.byType(NavigationBar), findsNothing);
-    expect(find.byTooltip('Show workspace controls'), findsOneWidget);
-    await tester.tap(find.byTooltip('Show workspace controls'));
-    await tester.pumpAndSettle();
-    expect(find.byType(NavigationBar), findsOneWidget);
-  });
